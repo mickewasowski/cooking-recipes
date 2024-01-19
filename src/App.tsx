f435748be15b29714bb1a1fc69469113e94b57bf
Rosen Yorgov <39274188+mickewasowski@users.noreply.github.com>
2024-01-19 16:11:48 +0200

    recepe component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css'
 import NavBar from './components/misc/Navbar'
+import RecepeCard from './components/recepe/RecepeCard';
 
 function App() {
   return (
-    <NavBar></NavBar>
+    <>
+      <NavBar />
+      <RecepeCard />
+    </>
   )
 }
 
