940edad61d87df6ac862171168eaa21a36c335b8
Rosen Yorgov <39274188+mickewasowski@users.noreply.github.com>
2024-01-21 13:06:03 +0200

    added root reducer type

--- src/store/root-reducer.ts
+++ src/store/root-reducer.ts
@@ -5,3 +5,5 @@ export const rootReducer = combineReducers({
     //TODO: add the user reducer and the recipe reducer here
     user: userReducer,
 });
+
+export type IRootState = ReturnType<typeof rootReducer>
