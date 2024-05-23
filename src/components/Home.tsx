fe1d160fa4005d9c942b901df2e433fa0cbef285
Rosen Yorgov <yorgov.h.r@gmail.com>
2024-05-23 21:13:27 +0300

    home page UI rework in progress

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -9,6 +9,7 @@ import { getLatestRecipiesStart } from '../store/recipe/recipe.action';
 
 function HomePage() {
   const dispatcher = useDispatch();
+  //TODO: take the latest 3 added
   const allRecipies = useSelector((state: IRootState) => getLatestRecipes(state.recipe));
 
   useEffect(() => {
@@ -16,17 +17,34 @@ function HomePage() {
   }, []);
 
   return (
-    <div>
+    <div className='home-wrapper'>
       <div>
-        <h2>Welcome to MyRecipes!</h2>
-        <SearchBar />
-        <h2>Popular Recipes</h2>
-        <div>
-          {
-            allRecipies?.map(recipe => (
-              <RecipeCard key={recipe?.id} {...recipe} />
-            ))
-          }
+        <h1 className='main-heading'>Unlock you appetite</h1>
+        <div className='text-carousel'>
+          <div className='animation-container'>
+            <div>
+              <p>Salads</p>
+            </div>
+            <div>
+              <p>Soups</p>
+            </div>
+            <div>
+              <p>Desserts</p>
+            </div>
+            <div>
+              <p>Vegan</p>
+            </div>
+          </div>
+        </div>
+        <div className='recipes-container'>
+          <h2>Checkout our user's latest entries</h2>
+          <div className='recipes-wrapper'>
+            {
+              allRecipies?.map(recipe => (
+                <RecipeCard key={recipe?.id} {...recipe} />
+              ))
+            }
+          </div>
         </div>
       </div>
     </div>
