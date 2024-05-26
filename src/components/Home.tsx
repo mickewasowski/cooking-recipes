af662d027ee47d6d8f7d874f37ea43f631ef9199
Rosen Yorgov <yorgov.h.r@gmail.com>
2024-05-26 12:40:36 +0300

    reworked home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -17,32 +17,36 @@ function HomePage() {
   return (
     <div className='home-wrapper'>
       <div className='inner-container'>
-        <h1 className='main-heading'>Unlock you appetite</h1>
-        <div className='text-carousel'>
-          <div className='animation-container'>
-            <div>
-              <p>Salads</p>
-            </div>
-            <div>
-              <p>Soups</p>
-            </div>
-            <div>
-              <p>Desserts</p>
-            </div>
-            <div>
-              <p>Vegan</p>
+        <div className='hero-text-wrapper'>
+          <div>
+            <h1 className='main-heading'>Unlock you appetite</h1>
+            <div className='text-carousel'>
+                <div className='animation-container'>
+                  <div>
+                    <p>Salads</p>
+                  </div>
+                  <div>
+                    <p>Soups</p>
+                  </div>
+                  <div>
+                    <p>Desserts</p>
+                  </div>
+                  <div>
+                    <p>Vegan</p>
+                  </div>
+                </div>
             </div>
           </div>
         </div>
-        <div className='recipes-container'>
-          <h2>Checkout our user's latest entries</h2>
-          <div className='recipes-wrapper'>
-            {
-              allRecipies?.map(recipe => (
-                <RecipeCard key={recipe?.id} {...recipe} />
-              ))
-            }
-          </div>
+      </div>
+      <div className='recipes-container'>
+        <h2>Checkout our latest entries</h2>
+        <div className='recipes-wrapper'>
+          {
+            allRecipies?.map(recipe => (
+              <RecipeCard key={recipe?.id} {...recipe} />
+            ))
+          }
         </div>
       </div>
     </div>
